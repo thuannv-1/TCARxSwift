a347a649101680b0382eb314635597bfd0a540d8
tk1 <thuan2609.tk1@gmail.com>
2023-10-23 21:05:14 +0700

    Migrate Target

--- Example/TCARxSwift/ViewController.swift
+++ Example/TCARxSwift/ViewController.swift
@@ -7,18 +7,21 @@
 //
 
 import UIKit
+import TCARxSwift
 
 class ViewController: UIViewController {
+    
+    var testView = CAView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(testView)
+        testView.frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 300.0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
 
